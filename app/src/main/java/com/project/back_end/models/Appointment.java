3b462240c3a4e3a51c3b4ee891ab5d948e4901c7
kleber89 <benkosisackson@gmail.com>
2026-01-07 14:29:31 -0500

    Update Appointment class

--- app/src/main/java/com/project/back_end/models/Appointment.java
+++ app/src/main/java/com/project/back_end/models/Appointment.java
@@ -7,8 +7,11 @@ import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Transient;
 
 @Entity
 public class Appointment {
